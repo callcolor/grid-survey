0498b764e85a3f933a97696764da8ff4b732c0dc
jerome <jerome@kansascitygeek.com>
2020-08-19 20:56:39 -0500

    init

---
+++ src/components/image.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useStaticQuery, graphql } from "gatsby";
+import Img from "gatsby-image";
+
+/*
+ * This component is built using `gatsby-image` to automatically serve optimized
+ * images with lazy loading and reduced file sizes. The image is loaded using a
+ * `useStaticQuery`, which allows us to load the image from directly within this
+ * component, rather than having to pass the image data down from pages.
+ *
+ * For more information, see the docs:
+ * - `gatsby-image`: https://gatsby.dev/gatsby-image
+ * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
+ */
+
+const Image = (props: { src: string, alt: string }): JSX.Element => {
+  const data = useStaticQuery(graphql`
+      query {
+        images: allFile {
+          edges {
+            node {
+              relativePath
+              name
+              childImageSharp {
+                fluid(maxWidth: 1920) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+          }
+        }
+      }
+  `);
+
+  const image = data.images.edges.find(n => {
+    return n.node.relativePath === props.src;
+  });
+  if (!image) { return null; }
+
+  return <Img fluid={image.node.childImageSharp.fluid} />;
+}
+
+export default Image;
